2fa035f46a39dd285b605efaf84568f0d5e4c623
zhouxuan2017 <1207469303@qq.com>
2019-10-22 19:14:22 +0800

    a

--- node-zuoye1/js/zuoye.js
+++ node-zuoye1/js/zuoye.js
@@ -2,23 +2,28 @@
 
 const http = require('http'),
     fs = require('fs'),
-    qs = require('querystring');
+    path = require('path'),
+    url = require('url'),
+     qs = require('querystring');
 
-var items = [];
+
+     var items = [];
 
 http.createServer(function (req, res) {
+  
     if (req.url != '/') { return err(res); }
 
-    console.log(req.headers);
-    console.log('');
+    //   console.log(req.headers);
+    //   console.log('');
 
     switch (req.method) {
         case 'GET':
-            show(res);
+            show(req, res);
             break;
         case 'POST':
             add(req, res);
             break;
+
         default:
             err(res);
             break;
@@ -32,31 +37,87 @@ function err(res) {
         'Content-Type': 'text/plain'
     });
     res.end(msg);
+}
+var item=['list']
+function show(req, res) {
+    qs.parse(url.parse(req.url).query).item
+    console.log(req.route)
+ if(req.url=='/')
+ {
+   
+    var html = fs.readFileSync('.././chapter.html').toString('utf8'),
+    
+    items_html = items.map(function(item) {return '      <li>' + item + '</li>';}).join('\n');
+items.push('list')
+html = html.replace('%', items_html); 
 
+res.writeHead(200, {
+  'Content-Type': 'text/html',
+  
+  'Access-Control-Allow-Origin': '*'
+
+});
+
+res.end(html);
+ }
+ 
+
+    // if (path.extname(pathname) == '') {
+    //     pathname += '/'
+    // }
+    // if (pathname.charAt(pathname.length - 1) == '/') {
+    //     pathname += '.././chapterList.html'
+    // }
+    // path.exists(pathname, function (exists) {
+    //     if (exists) {
+    //         switch (pathname) {
+    //             case '.html':
+    //                 res.writeHead(200, {
+    //                     'Content-Type': 'text/html',
+    //                     'Access-Control-Allow-Origin': '*'
+    //                 });
+    //             case '.css':
+    //                 res.writeHead(200, {
+    //                     'Content-Type': 'text/css',
+    //                     'Access-Control-Allow-Origin': '*'
+    //                 });
+    //             case '.js':
+    //                 res.writeHead(200, {
+    //                     'Content-Type': 'text/js',
+    //                     'Access-Control-Allow-Origin': '*'
+    //                 });
+    //             case '.jpg':
+    //                 res.writeHead(200, {
+    //                     'Content-Type': 'image/jpg',
+    //                     'Access-Control-Allow-Origin': '*'
+    //                 });
+    //             case '.png':
+    //                 res.writeHead(200, {
+    //                     'Content-Type': 'image/png',
+    //                     'Access-Control-Allow-Origin': '*'
+    //                 });
+    //             default:
+    //                 res.writeHead(200, { "Content-Type": "application/octet-stream" });
+    //         }
+    //         fs.readFile(pathname,function (err,data){
+    //             res.end(data);
+    //     })
+  
+  
+
+
+// })
 }
 
-function show(res) {
-    var html = fs.readFileSync('../chapter.html').toString('utf8'),
-        items_html = items.map(function (item) { return '      <li>' + item + '</li>'; }).join('\n');
-
-    html = html.replace('%', items_html);
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Content-Length': Buffer.byteLength(html),
-        'Access-Control-Allow-Origin': '*'
-    });
-    res.end(html);
-
-}
 
 function add(req, res) {
     var body = '';
+
     req.on('data', function (chunk) { body += chunk; });
     req.on('end', function () {
         var item = qs.parse(body).item;
         if (item !== '') { items.push(item); }
+
         show(res);
-
     });
-
 }
