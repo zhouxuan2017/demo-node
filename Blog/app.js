b19a251eeefcc4531cd558bf5acbf5f93c5f7afd
zhouxuan <1207469303@qq.com>
2019-11-11 10:53:52 +0800

    add Blog

--- Blog/app.js
+++ Blog/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser=require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use(express.static(__dirname))
+router.get('/', function(req, res) {
+   res.sendFile(__dirname+'/login.html') ;
 });
-
+router.post('/login',function(req,res){
+  res.status=200;
+  })
 module.exports = router;
