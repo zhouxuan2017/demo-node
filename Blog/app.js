fcae3ff8e399dea3c286126b7f2e138a377885bb
zhouxuan <1207469303@qq.com>
2019-11-11 15:10:01 +0800

    blog

--- Blog/app.js
+++ Blog/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true})) 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -4,7 +4,26 @@ router.use(express.static(__dirname))
 router.get('/', function(req, res) {
    res.sendFile(__dirname+'/login.html') ;
 });
+router.get('/list',function(req,res){
+  res.sendFile(__dirname+'/list.html')
+})
 router.post('/login',function(req,res){
-  res.status=200;
-  })
+    var post='';
+    req.on('data',function(data){
+      post+=data;
+    })
+    req.on('end',function(){
+      if(JSON.parse(post).username=='zhangsan'&&JSON.parse(post).pwd=='123'){
+        res.writeHead(200,{
+          "Content-Type":"text/plain"
+        });
+        res.end('ok');
+      }
+      else
+    {
+      res.statusCode=404;
+      res.end('No ok')
+    }
+    })
+})
 module.exports = router;
