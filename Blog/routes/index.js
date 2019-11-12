700c4f31aba42eb1c8aba813e4e46c1c69edeb23
zhouxuan2017 <1207469303@qq.com>
2019-11-12 14:02:07 +0800

    Blog end

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -1,29 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var arr=require('./data.json')
+var chapter=arr.chapterList;
+var user=arr.users;
+
 router.use(express.static(__dirname))
 router.get('/', function(req, res) {
    res.sendFile(__dirname+'/login.html') ;
 });
+//post请求判断登录名密码
+router.post('/login',function(req,res){
+  if(req.body.username==user[0].username&&req.body.pwd==user[0].password)
+  {
+    res.sendFile(__dirname+'/list.html')
+  }
+  else{
+    res.sendFile(__dirname+'/error.html')
+  }
+})
+
+//list
 router.get('/list',function(req,res){
   res.sendFile(__dirname+'/list.html')
 })
-router.post('/login',function(req,res){
-    var post='';
-    req.on('data',function(data){
-      post+=data;
-    })
-    req.on('end',function(){
-      if(JSON.parse(post).username=='zhangsan'&&JSON.parse(post).pwd=='123'){
-        res.writeHead(200,{
-          "Content-Type":"text/plain"
-        });
-        res.end('ok');
-      }
-      else
-    {
-      res.statusCode=404;
-      res.end('No ok')
-    }
-    })
+
+//增加chapterlist
+router.get('/add',function(req,res){
+    var bb = JSON.stringify(chapter);
+  res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(bb),
+      'Content-Type': 'text/plain;charset="utf-8"'
+  })
+  res.end(bb)
 })
 module.exports = router;
